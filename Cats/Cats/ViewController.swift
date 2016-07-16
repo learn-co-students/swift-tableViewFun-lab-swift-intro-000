40fdc04dcc0ad792a80e30b60c276edf0d2ed0b8
Michael Dippery <michael@monkey-robot.com>
2016-07-16 15:35:41 -0700

    Revert "Complete implementation of Cats app"
    Might be useful to store a solution in this repo, but we want the
    students to start with a blank slate.
    
    This reverts commit 60edfe9f2ca0c38a9fdc485536ec03b978ea19fd.

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -9,39 +9,4 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    let catBreeds = [
-        "British Shorthair",
-        "Siamese",
-        "Persian",
-        "Abyssinian",
-        "Maine Coon",
-        "Bengal",
-        "Sphynx",
-        "Ragdoll",
-        "American Shorthair",
-        "Exotic Shorthair",
-        "Burmese",
-        "Scottish Fold",
-        "Birman",
-        "Norwegian Forest",
-    ].sort()
-}
-
-// MARK: - Table View Data Source
-
-extension ViewController {
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return catBreeds.count
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("KittyCell", forIndexPath: indexPath)
-        let breed = catBreeds[indexPath.row]
-        cell.textLabel?.text = breed
-        return cell
-    }
 }
