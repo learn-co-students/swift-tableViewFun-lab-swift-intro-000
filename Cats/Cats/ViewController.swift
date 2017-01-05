c481e56b957de510c5945793beb72e91f0f93742
richardsonjm <richardsonjm@gmail.com>
2017-01-05 10:55:17 -0500

    Outdoor cats are best

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    let catBreeds = [
+        "Itchy",
+        "Scatchy",
+        "Meany",
+        "Sneezy",
+        "Lazy",
+        "Did I say mean already?"
+    ]
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
-   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catBreeds.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let catBreed = catBreeds[indexPath.row]
+        cell.textLabel?.text = catBreed
+        return cell
+    }
     
 }
