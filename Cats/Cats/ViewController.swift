eef98b467929056a464e5b8158816b475e2e963c
JJJV <juan@jaramillo.net>
2017-02-04 18:53:17 -0600

    Lab solution

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,50 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var cats: [String] = []
     
-   
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        generateList()
+        
+    }
     
+    func generateList() {
+        
+        cats = [
+            "Abyssinian",
+            "American Shorthair",
+            "Bengal",
+            "Birman",
+            "British Shorthair",
+            "Burmese",
+            "Exotic Shorthair",
+            "Maine Coon",
+            "Norwegian Forest",
+            "Persian",
+            "Ragdoll",
+            "Chesterfield"
+        ]
+        
+    }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cats.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let catBreed = cats[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = catBreed
+        print("\(indexPath.section) -- \(indexPath.row)")
+        return cell
+    }
 }
