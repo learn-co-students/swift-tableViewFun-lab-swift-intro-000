3711d23b5593cc83844db4b5e9d17f1b86ca83d3
Ryan Hennings <r.hennings11@gmail.com>
2017-02-16 21:24:55 -0800

    Kat lab complete

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,6 +10,60 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    @IBOutlet var katTableView: UITableView!
+    
+    
+    var kats: [String] = []
+    
+    override func viewDidLoad() {
+        setKats()
+        print(kats)
+        sortKats(&kats)
+        
+        print(kats)
+        
+        
+    }
+    
+    func setKats() {
+        kats = [
+            "Siamese",
+            "Persian",
+            "Maine Coon",
+            "Ragdoll",
+            "Bengal",
+            "Himalayan",
+            "American Shorthair",
+            "Manx",
+            "Russian Blue",
+            "Sphynx"
+        ]
+    }
+    
+    func sortKats(_ katList: inout [String]) {
+        katList.sort()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kats.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kCell = katTableView.dequeueReusableCell(withIdentifier: "katCell", for: indexPath)
+        
+        let kat = kats[indexPath.row]
+        
+        kCell.textLabel?.text = kat
+        
+        return kCell
+    }
+    
+    
+    
     
    
     
