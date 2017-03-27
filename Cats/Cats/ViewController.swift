ce75b85f5c281f511c98c7b5654f72b3ba4a65ee
Brendt Bly <brendtbly@Brendts-MacBook-Pro.local>
2017-03-27 13:03:04 -0500

    Done

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    let catbreeds = ["Abyssinian",
+                     "American Shorthair",
+                     "Bengal",
+                     "Birman",
+                     "British Shorthair"]
     
-   
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catbreeds.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let breed = catbreeds[indexPath.row]
+        cell.textLabel?.text = breed
+        return cell
+    }
 }
