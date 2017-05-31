6deb0b62f5140d8913c5277662f4fcb37154ca56
Morgan McCauley <morgan.mccauley94@gmail.com>
2017-05-31 22:59:55 +1200

    cat lab

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var cats = [
+        "cat1",
+        "cat2",
+        "cat3",
+        "cat4",
+        "cat5",
+        "cat6",
+        "cat7",
+        "cat8",
+        "cat9"
+    ]
     
-   
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cats.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let catName = cats[indexPath.row]
+        cell.textLabel?.text = catName
+        return cell
+    }
     
     
 }
