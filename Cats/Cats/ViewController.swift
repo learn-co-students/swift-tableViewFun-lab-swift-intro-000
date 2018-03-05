0e2a9ee01905d4147ab332b8bb3df5d80de6d2d9
nikandv <nikandrov.ant@ya.ru>
2018-03-05 12:55:55 +0300

    Complete

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    
+    var catTypes: [String] = [ "Persian", "Persian", "Persian", "Persian", "Persian", "Persian",
+                               "Persian", "Persian", "Persian", "Persian", "Persian", "Persian",
+                               "Persian", "Persian", "Persian", "Persian", "Persian", "Persian",
+                               "Persian", "Persian", "Persian", "Persian", "Persian", "Persian" ]
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catTypes.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let catType = catTypes[indexPath.row]
+        cell.textLabel?.text = catType
+        return cell
+    }
     
 }
