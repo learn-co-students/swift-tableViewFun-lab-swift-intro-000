5f33391b1d9b58c6063da4b4748e99d4adec974e
bodeacorina <corina.bodea@yahoo.com>
2018-10-24 11:59:34 +0300

    Add tableViewFun Lab solution

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    
+    let reuseIdentifier = "KittyCell"
+
+    let kittyBreeds = [
+        "Abyssinian",
+        "American Shorthair",
+        "Bengal",
+        "Birman",
+        "British Shorthair",
+        "Burmese",
+        "Exotic Shorthair",
+        "Maine Coon",
+        "Norwegian Forest",
+        "Persian",
+        "Ragdoll",
+    ]
    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kittyBreeds.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let songTitle = kittyBreeds[indexPath.row]
+        cell.textLabel?.text = songTitle
+        return cell
+    }
     
     
 }
